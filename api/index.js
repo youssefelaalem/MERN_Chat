7f0ae905e6759e18c5eb4f2202aea6e4b485f166
youssefelaalem <youssefelaalem82@gmail.com>
2025-04-13 12:47:17 +0200

    solve CORS Issue

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ app.use(express.json()); //to receive json from the body
 app.use(cookieParser()); //
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.REMOTE_CLEINT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"], // Add 'Authorization' if necessary
