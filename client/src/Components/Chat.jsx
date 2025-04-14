234ac27d7f2fb6436b8b100fb114df173006ba66
youssefelaalem <youssefelaalem82@gmail.com>
2025-04-14 13:31:56 +0200

    rebuild app

--- client/src/Components/Chat.jsx
+++ client/src/Components/Chat.jsx
@@ -82,7 +82,7 @@ export default function Chat({ selectedUserIdFromRoute }) {
     connectionWS();
   }, [selectedUserId]);
   function connectionWS() {
-    // const ws = new WebSocket("ws://localhost:8080");
+    // const ws = new WebSocket("ws://localhost:8080"); 
     const ws = new WebSocket("wss://mernchat-production-815e.up.railway.app");
     setWs(ws);
     ws.addEventListener("message", handleMessage);
